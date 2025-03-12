e61a29925b9173945b27a26a263c32aceee17c33
rogereriksen1980 <45829469+rogereriksen1980@users.noreply.github.com>
2025-03-12 12:55:06 +0100

    Create MemberForm.js

---
+++ frontend/src/components/members/MemberForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MemberForm = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold mb-6">Add/Edit Member</h1>
+      <p>Member form will be displayed here.</p>
+    </div>
+  );
+};
+
+export default MemberForm;
