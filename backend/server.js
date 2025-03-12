02f1fce817f18c91b35566825fefdf78338b08f2
rogereriksen1980 <45829469+rogereriksen1980@users.noreply.github.com>
2025-03-12 12:16:05 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,10 @@ app.use('/api/tasks', taskRoutes);
 
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, 'public')));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
 }
 
