c0b57d011491cd908585ace044b47670be6fe84b
rogereriksen1980 <45829469+rogereriksen1980@users.noreply.github.com>
2025-03-12 12:55:41 +0100

    Create MeetingForm.js

---
+++ frontend/src/components/meetings/MeetingForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MeetingForm = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold mb-6">Schedule Meeting</h1>
+      <p>Meeting form will be displayed here.</p>
+    </div>
+  );
+};
+
+export default MeetingForm;
