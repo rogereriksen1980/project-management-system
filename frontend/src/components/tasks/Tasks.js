4140e6795a440b07c1d7c311836d7f6c601bb9b1
rogereriksen1980 <45829469+rogereriksen1980@users.noreply.github.com>
2025-03-12 12:55:56 +0100

    Create Tasks.js

---
+++ frontend/src/components/tasks/Tasks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tasks = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold mb-6">Tasks</h1>
+      <p>Tasks will be displayed here.</p>
+    </div>
+  );
+};
+
+export default Tasks;
