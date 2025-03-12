7fe21ec8597722c8f1477072c60ad4f1b63f8c99
rogereriksen1980 <45829469+rogereriksen1980@users.noreply.github.com>
2025-03-12 12:54:02 +0100

    Create Projects.js

---
+++ frontend/src/components/projects/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold mb-6">Projects</h1>
+      <p>Projects will be displayed here.</p>
+    </div>
+  );
+};
+
+export default Projects;
